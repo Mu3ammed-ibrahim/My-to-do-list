bc21c40bc47559c3c787ea6a7bc7bbb073b00987
Muhammed <www.tota.11@gmail.com>
2024-06-12 21:53:04 +0300

    adding comments for documentation

--- project.js
+++ project.js
@@ -1,39 +1,41 @@
 
 // To DO list project 
-
+//declareing needed variables
 let input = document.querySelector('.input');
 let sumbit = document.querySelector('.add');
 let tasksContainer = document.querySelector('.tasks');
 
-sumbit.addEventListener('click', add_Tasks)
 function add_Tasks(){
-  if(input.value !== ""){
-    let newTask = document.createElement('div');
-    let taskName = document.createElement('h2');
-    let del = document.createElement('input');
-    taskName.innerHTML=`${input.value}`;
+    if(input.value !== ""){
+        let newTask = document.createElement('div');
+        let taskName = document.createElement('h2');
+        let del = document.createElement('input');
+        taskName.innerHTML=`${input.value}`;
     del.setAttribute('type', 'submit');
     del.setAttribute('value', 'delete');
- //styling
+    //styling
     del.style.cssText = 'color:white; padding:10px; border:none; border-radius:5px; background-color:red;'
     taskName.style.cssText = 'font-family: Courier New, Courier, monospace; '
     newTask.style.cssText ='margin:20px; background-color:aliceblue;  border-radius: 10px; border: none; padding:10px 10px; display: flex; justify-content: space-between;'
     window.localStorage.setItem('tasks',input.value);
-    
+    // append to body
     newTask.appendChild(taskName)
-      newTask.appendChild(del)
+    newTask.appendChild(del)
       tasksContainer.appendChild(newTask);
-    
+    //clear the search field
       input.value = "";
       del.addEventListener("click",   function(){
-        newTask.remove();
-        window.localStorage.removeItem('tasks')
-      })
-    }
-}
+          newTask.remove();
+          window.localStorage.removeItem('tasks')
+          })
+          }
+          }
 
-
-
-
-
-
+          //creating tasks list
+          sumbit.addEventListener('click', add_Tasks)
+          
+          
+          
+          
+          
+          
